acb036fab0bc89cf4d0fce86393b45796bdcfcd5
mdjunaidjewel <mdjunaidjewell@gmail.com>
2026-01-09 15:42:07 +0600

    4no number problem solve now

--- problem-04.js
+++ problem-04.js
@@ -7,3 +7,19 @@ Input: [5, 1, 9, 3]
 Output: 9
 
 */
+
+function findMax(numbers) {
+  let max = numbers[0];
+
+  for (let num of numbers) {
+    if (num > max) {
+      max = num;
+    }
+  }
+
+  return max;
+}
+
+// Example usage
+console.log(findMax([5, 1, 9, 3]));
+
